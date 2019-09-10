b1d12439f7328b93fc8a3e52c0025f502524fe00
IN303321 <IN303321@ACH-D7043-000.india.accretivehealth.local>
2019-09-10 15:09:58 +0530

    commit

---
+++ R1Contact/src/test/java/r1/commons/R1ContactCommonMethods.java
@@ -0,0 +1,62 @@
+package r1.commons;
+
+import static org.junit.Assert.assertFalse;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import junit.framework.Assert;
+import net.serenitybdd.core.annotations.findby.By;
+import net.serenitybdd.core.annotations.findby.FindBy;
+import net.serenitybdd.core.pages.WebElementFacade;
+
+public class R1ContactCommonMethods extends BasePage {
+
+	public ArrayList colValues;
+	public ArrayList columnValue;
+	
+	
+	
+	
+	
+/*................................... Get TABLE COLUMN VALUE  .........................................*/
+
+	
+    @SuppressWarnings("deprecation")
+	public void getTableColValue(String rowLocator, String colLocator, String accountName) 
+	{
+    	boolean flag=false;
+		colValues = new ArrayList();
+		int rowSize = findAll(By.xpath(rowLocator)).size();
+		int colSize = findAll(By.xpath(colLocator)).size();
+		for (int i = 1; i <= rowSize; i++)
+		{
+			String rowlocator1 = rowLocator + "[" + i + "]" ;
+			
+			for(int j=1;j<colSize;j++)
+			{
+				String colLocator1=rowlocator1+"/td["+j+"]";
+				String accountNumber=element(By.xpath(colLocator1)).getText();
+				if(accountNumber.equalsIgnoreCase(accountName))
+				{
+						element(rowlocator1+"/following::a[1]//div").click();
+						flag=true;
+						break;
+					
+				}else
+					{
+						flag=false;
+					}
+
+				}
+			if(flag)
+			{
+				break;
+			}
+
+	     }
+					
+		}
+
+}
+
---
+++ R1Contact/src/test/java/r1/steps/definitions/ReminderHeaderStepDef.java
@@ -0,0 +1,25 @@
+package r1.steps.definitions;
+
+import cucumber.api.java.en.Given;
+import r1.commons.BasePage;
+import r1.pages.HomePage;
+import r1.pages.ReminderPage;
+
+public class ReminderHeaderStepDef extends BasePage {
+
+	HomePage home;
+	ReminderPage rem;
+	
+	@Given("^user is on R1Contact home page$")
+	public void user_is_on_R_Contact_home_page() {
+		OpenBrowser();
+	}
+
+	@Given("^Verify the header in 'Reminders' section$")
+	public void verify_the_header_in_Reminders_section() {
+		home.switchHeaderFrame();  
+		home.reminderHeader();
+	}
+	
+
+}
